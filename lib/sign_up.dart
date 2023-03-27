a961f8155c218911b8d1c025e4aa4e414bfc0c25
amroaboamro <amer.abuamer444@gmail.com>
2023-03-27 23:02:03 +0300

    first

---
+++ lib/sign_up.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'login.dart';
+import 'package:head_gasket/Widget/background.dart';
+
+final dio = Dio();
+addUser(name,email, password, phone,carModel) async {
+  return await dio.post('',
+      data: {"name": name, "email":email,"password": password, "phone": phone,"carModel":carModel},
+      options: Options(contentType: Headers.formUrlEncodedContentType));
+}
+
+
+
+class SignUpScreen extends StatefulWidget{
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  String? _selectedCarType;
+
+  List<String> _dropdownItems = [
+    'Hyundai accent',
+    'Hyundai elantra',
+    'Seat leon ',
+    'BMW 320i',
+    'BMW m4',
+    'Skoda octavia',
+    'Skoda Combi',
+    'Skoda superb',
+    'volkswagen golf',
+    'volkswagen polo',
+    'volkswagen tiguan',
+    'Mercedes G-class',
+    'Mercedes Benz E350',
+    'Mercedes E350',
+    'Kia sportage',
+    'kia rio',
+
+  ];
+
+  @override
+  var name,email, password, phone, carModel;
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Background(
+       child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: size.width*0.1),
+              child: Text(
+                "SIGN UP",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color:mainColor,
+                  fontSize: 35,
+
+                ),
+                textAlign: TextAlign.left,
+
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: size.width*0.1, vertical: 10),
+              child: TextField(
+                onChanged: (value){
+                  name=value;
+                },
+                decoration: InputDecoration(
+                    labelText: "Name",
+                  prefixIcon:  Icon(Icons.account_circle_outlined),
+                ),
+
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: size.width*0.1, vertical: 10),
+              child: TextField(
+                onChanged: (value){
+                  email=value;
+                },
+                decoration: InputDecoration(
+                    labelText: "Email",
+                  prefixIcon: Icon(Icons.email_outlined),
+                ),
+
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: size.width*0.1, vertical: 10),
+              child: TextField(
+                onChanged: (value){
+                  password=value;
+                },
+                decoration: InputDecoration(
+                    labelText: "Password",
+                  prefixIcon: Icon(Icons.vpn_key_outlined),
+
+
+                ),
+                obscureText: true,
+
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: size.width*0.1, vertical: 10),
+
+              child: TextField(
+                onChanged: (value){
+                  phone=value;
+                },
+                decoration: InputDecoration(
+                  labelText: "Phone",
+                  prefixIcon: Icon(Icons.phone),
+
+
+                ),
+
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: size.width*0.1, vertical: 10),
+              child:   InputDecorator(
+                decoration: InputDecoration(
+                  labelText: 'Car Model:',
+                  prefixIcon: Icon(Icons.car_rental_rounded),
+                  border: OutlineInputBorder(),
+
+                ),
+                child:DropdownButton<String>(
+                value: _selectedCarType,
+                onChanged: (value) {
+                  setState(() {
+                    carModel = value;
+
+                  });
+                },
+                items: _dropdownItems.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.fromLTRB(0, 40, 0, 10),
+              child: RaisedButton(
+                onPressed: () {
+                  addUser(name,email, password, phone,carModel);
+                },
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 50.0,
+                  width: size.width * 0.5,
+                  decoration: new BoxDecoration(
+                      borderRadius: BorderRadius.circular(80.0),
+                      gradient: new LinearGradient(
+                          colors: gradientColors
+                      )
+                  ),
+                  padding: const EdgeInsets.all(0),
+                  child: Text(
+                    "SIGN UP",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: GestureDetector(
+                onTap: () => {
+
+                },
+
+                child: Row(
+                  mainAxisAlignment:MainAxisAlignment.center ,
+                  children:<Widget> [
+                    Text(
+                      "Already Have an Account? ",
+                      style: TextStyle(
+                        fontSize: 12,
+
+                        color: Colors.black,
+
+                      ),
+                    ),
+                    Container(
+                      child: GestureDetector(
+                        child: Text(
+                          "Sign in",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                            color: mainColor,
+
+
+                          ),
+
+                        ),
+                        onTap: ()=>{
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()))
+                        },
+
+                      ),
+
+                    ),
+
+                  ],
+                ),
+
+              ),
+            )
+
+
+
+
+          ],
+        ),
+        ),
+
+      ),
+    );
+
+  }
+}
+
